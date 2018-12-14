7615caeba8d96ba5c09a2588f56d17f8634921eb
falcon256 <256games@gmail.com>
2018-12-14 09:31:59 -0700

    Revert "working multiplayer"
    This reverts commit db6de8f4205c4e8067e8fd389cffb50e8dcb79ff.

--- Assets/Scripts/ToastOcRigIfNotLocal.cs
+++ Assets/Scripts/ToastOcRigIfNotLocal.cs
@@ -6,33 +6,18 @@ using UnityEngine.Networking;
 public class ToastOcRigIfNotLocal : NetworkBehaviour {
 
     public GameObject OcStuff = null;
-    public Camera mc = null;
-    public Camera mc1 = null;
-    public Camera mc2 = null;
-    // Use this for initialization
-    void Start () {
+
+	// Use this for initialization
+	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (!isLocalPlayer)
+		if(!isLocalPlayer)
         {
             if (OcStuff != null)
                 Destroy(OcStuff);
         }
-        else
-        {
-            if (mc1.tag!="MainCamera")
-            {
-                mc1.tag = "MainCamera";
-                mc1.enabled = true;             
-            }
-            if(mc2.tag!="MainCamera")
-            {
-                mc2.tag = "MainCamera";
-                mc2.enabled = true;
-            }
-        }
-    }
+	}
 }
